22f776507df1ce43050291f5e39ec337b767cbd7
aerte <62424399+aerte@users.noreply.github.com>
2023-11-25 10:56:43 +0100

    cpu

--- run_cifar.sh
+++ run_cifar.sh
@@ -49,7 +49,7 @@ do
         for i in $(seq "$start_client" 1 "$end_client")
         do
             if [ "$i" -lt "$num2" ]; then
-                gpu_index=2
+                gpu_index=3
             elif [ "$i" -ge "$num2" ]; then
                 gpu_index=3
             fi

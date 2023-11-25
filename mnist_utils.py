9df5e6ef92d86423547dd210ba105f3e54b258cc
aerte <62424399+aerte@users.noreply.github.com>
2023-11-25 11:52:42 +0100

    cpu

--- mnist_utils.py
+++ mnist_utils.py
@@ -315,12 +315,12 @@ def create_dir(conf):
 
 def create_cifar10_dir(conf):
     model_mom = "../exp_data/"
-    model_dir = model_mom + "/cifar10/version_1/"
+    model_dir = model_mom + "/cifar10/version_2/"
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
 
     conf.folder_name = "cifar10"
-    conf.dir_name = "version_1"
+    conf.dir_name = "version_2"
     return conf
 
 

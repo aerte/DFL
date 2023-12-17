8062fd7f8321090a6d9b96921f9534857e5604cd
aerte <62424399+aerte@users.noreply.github.com>
2023-12-17 12:48:12 +0100

    cpu

--- submit_job.sh
+++ submit_job.sh
@@ -5,7 +5,7 @@
 ### -- set the job Name --
 #BSUB -J cifar10
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 8
+#BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=2:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now

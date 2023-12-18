307ba5ecbbc60f37fddb74220ed1173d39df05ec
aerte <62424399+aerte@users.noreply.github.com>
2023-12-18 09:13:57 +0100

    cpu

--- run_cifar.sh
+++ run_cifar.sh
@@ -50,9 +50,9 @@ do
         for i in $(seq "$start_client" 1 "$end_client")
         do
           if [ "$i" -lt "$num2" ]; then
-                gpu_index=0
+                gpu_index=2
             elif [ "$i" -ge "$num2" ] && [ "$i" -lt "$num3" ]; then
-                gpu_index=0
+                gpu_index=3
             fi
             echo "|GPU INDEX|CLIENT INDEX|${gpu_index}|${i}"
             export CUDA_VISIBLE_DEVICES="$gpu_index"
